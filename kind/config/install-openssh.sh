4bd798597dff489a94cf9caddab9039c6f3acc41
InHwan Jung <inhwan.jung@gmail.com>
2022-05-06 13:41:56 +0900

    Updated

--- kind/config/install-openssh.sh
+++ kind/config/install-openssh.sh
@@ -7,16 +7,21 @@ cat >install-openssh.sh<<EOF
 # yum install -y openssh-server openssh-clients openssh-askpass
 
 # ubuntu 18.04
-apt-get update
-apt-get install openssh-server
+apt-get update -y 
+apt-get install -y openssh-server
 
+echo "/etc/ssh/ssh_config"
 sed -i '/StrictHostKeyChecking/s/^#[ \t]*//g' /etc/ssh/ssh_config
-sed -i 's/StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
-sed -i 's/StrictHostKeyChecking True/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
-cat /etc/ssh/ssh_config | grep StrictHostKeyChecking
+# sed -i 's/StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+# sed -i 's/StrictHostKeyChecking True/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+sed -i '/#   PasswordAuthentication yes/s/^#[ \t]*//g' /etc/ssh/ssh_config
+cat /etc/ssh/ssh_config | grep -v ^# | grep -v ^$
 
-sed -i '/PermitRootLogin yes/s/^#[ \t]*//g' /etc/ssh/sshd_config
-cat /etc/ssh/sshd_config | grep PermitRootLogin
+echo ""
+echo "/etc/ssh/sshd_config"
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i '/#PasswordAuthentication yes/s/^#[ \t]*//g' /etc/ssh/sshd_config
+cat /etc/ssh/sshd_config | grep -v ^# | grep -v ^$
 
 systemctl restart ssh.service
 systemctl status sshd 
